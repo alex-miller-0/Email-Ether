dbbe1d08eebe816e30f3fdf55b9792e24bc33050
Alex Miller <asmiller1989@gmail.com>
2016-07-27 18:20:48 -0700

    switching directory for cleaner implementation

--- website/javascripts/app.js
+++ website/javascripts/app.js
@@ -1,16 +1,19 @@
 function register() {
   const email = document.getElementById("email").value;
   const url = "http://emailether.info:2777/send_email";
-
+  var regLabel = document.getElementById("registerResponse");
   // http request
   var r = new XMLHttpRequest();
   r.open('POST', url, true);
   r.setRequestHeader("Content-type", "application/json");
   r.onreadystatechange = function(){
     if (r.status == 200){
-      console.log("success!");
+      regLabel.color = "green";
+      regLabel.value = "Success! Please check your email."
     } else {
-      console.log("failed");
+      console.log("failed?")
+      regLabel.color = "red";
+      regLabel.innerHTML = "Registration failed."
     }
   }
 
